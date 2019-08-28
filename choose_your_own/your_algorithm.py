41bb5d069a517e166e1cb011ac74cb4ba8365458
Shaheryar <shaheryarmalik212@gmail.com>
2019-08-29 02:54:01 +0500

    Lesson 5 and 6 Complete

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -30,15 +30,17 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
+clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(features_train, labels_train)
+clf = clf.predict(features_test)
 
-
-
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+acc = accuracy_score(labels_test, clf)
+print acc
+#
+# try:
+#     #prettyPicture(clf, features_test, labels_test)
+# except NameError:
+#     pass
--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,5 +18,19 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+print "Total People", len(enron_data)
 
+poi_count = 0
+for person_name, featues in enron_data.iteritems():
+    print person_name, featues
+    if featues["poi"] is True:
+        poi_count = poi_count + 1
+print "POI Count: ", poi_count
 
+print "Stock belonging to James Prentice: ", enron_data["PRENTICE JAMES"]["total_stock_value"]
+
+print "Emails count from Wesley Colwell to poi: ", enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+
+print "The value of stock options exercised by Jeffrey K Skilling: ", enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+print len( [ enron_data[person]['total_payments'] for person in enron_data if (enron_data[person]['total_payments'] =='NaN') ])
