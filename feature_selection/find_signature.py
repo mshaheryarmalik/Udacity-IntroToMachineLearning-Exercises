1db642a4082a73a9ba89ff6debb1ff62e5a3f164
Shaheryar <shaheryarmalik212@gmail.com>
2019-09-06 10:31:39 +0500

    Chapter 12 and 13 Complete

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -8,9 +8,20 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
+words_file = "word_data_overfit.pkl"
 word_data = pickle.load( open(words_file, "r"))
+word_data2 = list()
+for i in word_data:
+	if "sshacklensf" in i: # second one is "cgermannsf"
+		i = i.replace("sshacklensf", "")
+	if "cgermannsf" in i:
+		i = i.replace("cgermannsf", "")
+	if "houectect" in i:
+		i = i.replace("houectect", "")
+	if "houect" in i:
+		i = i.replace("houect", "")
+	word_data2.append(i)
+authors_file = "email_authors_overfit.pkl"
 authors = pickle.load( open(authors_file, "r") )
 
 
@@ -20,7 +31,7 @@ authors = pickle.load( open(authors_file, "r") )
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
 from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data2, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
@@ -35,9 +46,19 @@ features_test  = vectorizer.transform(features_test).toarray()
 features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
 
-
+print len(features_train)
 
 ### your code goes here
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
-
+print accuracy_score(labels_test, pred)
+importances = clf.feature_importances_
+import numpy as np
+indices = np.argsort(importances)[::-1]
+print 'Feature Ranking: '
+for i in range(10):
+	print "{} feature no.{} ({})".format(i+1,indices[i],importances[indices[i]])
